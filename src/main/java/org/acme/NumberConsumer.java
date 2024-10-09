d2783dc849f9083e71c3bfb5c040afa39e78e099
ansahaRH <ansaha@redhat.com>
2024-10-09 09:52:48 -0700

    initial commit

---
+++ src/main/java/org/acme/NumberConsumer.java
@@ -0,0 +1,14 @@
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class NumberConsumer {
+    
+    private static final Logger LOG = Logger.getLogger(NumberConsumer.class);
+
+    @Incoming("numbers")
+    public void consume(Integer number) {
+        LOG.infof("Received number: %d", number);
+    }
+}
---
+++ src/main/java/org/acme/NumberResource.java
@@ -0,0 +1,30 @@
+package org.acme;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+import java.util.Random;
+
+@ApplicationScoped
+@Path("/numbers")
+public class NumberResource {
+
+    @Channel("generated-number")
+    Emitter<Integer> numberEmitter;
+
+    private Random random = new Random();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String triggerNumber() {
+        int randomNumber = random.nextInt(100);
+        numberEmitter.send(randomNumber);
+        return "Sent: " + randomNumber;
+    }
+}
