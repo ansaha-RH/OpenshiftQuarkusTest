b1c582ba44b5e98859a88b9c52d6773336a403cc
ansaha-RH <anish.saha@network.lilly.com>
2024-10-15 09:40:43 -0700

    again

--- src/main/java/org/acme/NumberResource.java
+++ src/main/java/org/acme/NumberResource.java
@@ -7,11 +7,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jboss.logging.Logger;
-import io.apicurio.registry.serde.jsonschema.JsonSchemaKafkaSerializer;
-import io.apicurio.registry.rest.client.RegistryClientFactory;
-import io.apicurio.registry.rest.v2.RegistryClient;
 
 import java.util.Random;
 import java.util.HashMap;
@@ -22,18 +18,11 @@ import java.util.Map;
 public class NumberResource {
 
     private static final Logger LOG = Logger.getLogger(NumberResource.class);
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    private final JsonSchemaKafkaSerializer<Map<String, Object>> serializer;
 
     @Channel("generated-number")
     Emitter<Map<String, Object>> numberEmitter;
 
     private Random random = new Random();
-
-    public NumberResource() {
-        RegistryClient client = RegistryClientFactory.create("http://apicurioregistry-mem.kafka-apicurio-test.router-default.apps.cluster-945vs.945vs.sandbox191.opentlc.com/");
-        serializer = new JsonSchemaKafkaSerializer<>(client, objectMapper);
-    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
